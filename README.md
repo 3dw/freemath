e5a6c7a39e08068fff831dbc21cd5ed76030e911
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-10-19 06:04:30 +0800

    debug

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue.freemath
+# freemath
 
 > A Vue.js project
 
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -104,7 +104,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     ]),
     // service worker caching
     new SWPrecacheWebpackPlugin({
-      cacheId: 'vue.freemath',
+      cacheId: 'freemath',
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
