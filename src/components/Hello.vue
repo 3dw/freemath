4bc4fd21758829305b45663a72f04c6147fbc5c5
Bestian Tang 唐宗浩 <besitan@gmail.com>
2019-05-11 13:44:25 +0800

    add note to parents

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -88,6 +88,7 @@ export default {
     return {
       s: '',
       news: [
+        '2019.05<a href="https://docs.google.com/document/d/14j0lEEZH5A1FNum7L6p9U1ETWx_0JTyasahIi9DLe3w/edit?usp=sharing" target="_blank">給家長的教學引導</a>上線了！',
         '2018.10.自由數學更新版上線，歡迎使用、協作、樂捐支持！',
         '2018.10.新功能：您可以直接在教材頁面上留言提問與校誤',
         '2018.10.全部教材搬上<a href="https://drive.google.com/drive/folders/0B-3AeNXITt7ZaExCaDkxNlZxUXc" target="_blank">GOOGLE雲端硬碟</a>, 改為GOOGLE DOC']
