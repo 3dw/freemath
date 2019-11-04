4eac88ccac1cde725e74259cf3b249ff0ffe622d
Bestian Tang <bestian@gmail.com>
2019-11-05 06:44:46 +0800

    test new ga

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -72,7 +72,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency',
       serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
-        './service-worker-prod.js'))}</script>`
+        './service-worker-prod.js'))}</script>`,
+      ga: env.GA
     }),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  GA: 'UA-26178243-4'
 }
