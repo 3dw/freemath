8a2c52a73801576510ef67dde451b80d59ad9095
Bestian Tang <bestian@gmail.com>
2020-03-17 04:48:10 +0800

    homepage better

--- src/components/New.vue
+++ src/components/New.vue
@@ -2,14 +2,21 @@
   .hello
     h1.ui.header 歡迎
     .ui.divider
-      .ui.segment.container.center.aligned
-        .ui.button.group
-          router-link.ui.huge.green.button(to="/hello")
-            i.search.icon
-            | 查詢教材
-          router-link.ui.huge.orange.button(to="/faq")
-            i.question.icon
-            | 常見問題
+    .ui.segment.container.center.aligned
+      .ui.button.group
+        router-link.ui.huge.green.button(to="/hello")
+          i.search.icon
+          | 查詢教材
+        router-link.ui.huge.orange.button(to="/faq")
+          i.question.icon
+          | 常見問題
+      .ui.button.group
+        a.ui.huge.purple.button(href="https://docs.google.com/document/d/1xUDSZPP1lmReEpAOhCXKUEln105MrVjFo05E4FcpMx0/edit?usp=drive_web&ouid=109123650148645242011", target="_blank")
+          i.user.icon
+          | 小學數學
+        a.ui.huge.teal.button(href="https://docs.google.com/document/d/1lw-1BIsl9uLPfphIQ_Ns4xbpLhE7D_KEn9B7Tomjrsw/edit", target="_blank")
+          i.users.icon
+          | 國中數學
 </template>
 
 <script>
@@ -30,6 +37,12 @@ export default {
 
 .hello {
   text-align: center;
+  height: 100vh;
+  background-image: url(/static/img/bg-img.jpeg);
+}
+
+.ui.segment {
+  background-color: #c9ffc9;
 }
 
 </style>
