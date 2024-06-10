5a81a180b6c5a9277a4a110ec4666f33e8e5e389
leechiuhui <107933615+leechiuhui@users.noreply.github.com>
2024-06-10 22:21:42 +0800

    影片檔案供上傳到自主學習促進會youtube頻道

--- src/components/New.vue
+++ src/components/New.vue
@@ -16,7 +16,7 @@
       iframe(
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/KMCPvosv-Uo"
+        src="https://www.youtube.com/embed/yMlfYi4nDS4"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen)
