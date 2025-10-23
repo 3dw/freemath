67245f4f960e9e440acc4bf8833927ddc0dbf2c8
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-10-23 15:46:10 +0800

    更新首頁，加入新影片

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -1,2 +1,5 @@
 // export const homePageSrcURL = 'https://www.youtube.com/embed/BTB5DdKOIHw?si=As0xp6f4dlw&list=PLebzuoh5ZI3LrVduRDqLcxCxlkruyDL27' 
-export const homePageSrcURL = 'https://www.youtube.com/embed/4rEqm-fh0SU?si=As0xp6f4dlw&list=PLebzuoh5ZI3LrVduRDqLcxCxlkruyDL27' 
+// export const homePageSrcURL = 'https://www.youtube.com/embed/4rEqm-fh0SU?si=As0xp6f4dlw&list=PLebzuoh5ZI3LrVduRDqLcxCxlkruyDL27' 
+
+export const homePageSrcURL = 'https://www.youtube.com/embed/Cw0kHk9E9t8?si=As0xp6f4dlw&list=PLebzuoh5ZI3LrVduRDqLcxCxlkruyDL27' 
+
